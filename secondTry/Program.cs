20b76447b49338ac37ac6784445030db751e0e98
Rahul Prasad <rahulprasad1399@gmail.com>
2026-08-17 20:30:38 +0530

    scope of local variable

--- secondTry/Program.cs
+++ secondTry/Program.cs
@@ -1,2 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[E]xit");
+Console.WriteLine("[R]emove a TODO");
+
+string? userChoice = Console.ReadLine(); 
+
+if(userChoice?.Length > 10)
+{
+    Console.WriteLine("Long answer");
+} else
+{
+    Console.WriteLine("Short answer");
+}
+
+// scope of local variable 
+int x = 10;
+{
+    int y = 20;
+    Console.WriteLine(x); // OK, x is visible here.
+}
+Console.WriteLine(y); // Error, y is out of scope here.
+
 Console.ReadKey();
+Console.ReadLine();
